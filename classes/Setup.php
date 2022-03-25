967085bb1858fea1a6b1d5888778a176b0e7e403
atEchoOff <gsus.p3nded@gmail.com>
2022-03-25 18:48:57 -0400

    Adding database config

---
+++ classes/Setup.php
@@ -0,0 +1,41 @@
+<?php
+
+spl_autoload_register(function ($classname) {
+    include "$classname.php";
+});
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+$db = new mysqli(Config::$db["host"], Config::$db["user"], Config::$db["pass"], Config::$db["database"]);
+
+$db->query("drop table if exists User;");
+$db->query("create table User (
+    id int not null auto_increment,
+    username text not null,
+    password text not null,
+    primary key (id)
+);");
+
+$db->query("drop table if exists Composition;");
+$db->query("create table Composition (
+    id int not null auto_increment,
+    user_id int not null,
+    name text not null,
+    primary key (id)
+);");
+
+$db->query("drop table if exists UserComposition;");
+$db->query("create table UserComposition (
+    id int not null auto_increment,
+    user_id int not null,
+    composition_id int not null,
+    primary key (id)
+);");
+
+$db->query("drop table if exists Recording;");
+$db->query("create table Recording (
+    id int not null auto_increment,
+    author_id int not null,
+    composition_id int not null,
+    primary key (id)
+);");
